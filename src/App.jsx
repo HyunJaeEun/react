071b7c44cba019830474a541b4ea3d5f34e178eb
HyunJaeEun <wodms2020@gmail.com>
2024-08-05 13:44:48 +0900

    firsh commit;

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Viewer from './assets/Viewer';
+import Controller from './assets/Controller';
+import { useState } from 'react';
+
+function App() {
+  const [count,setCount] = useState(0);
+
+  const onClickButton = (value) =>{
+    setCount(count+value) ;
+  }
+
+  return (
+    <div className='App'>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer count={count}/>
+      </section>  
+
+      <section>
+        <Controller onClickButton={onClickButton}/>
+      </section>
+    </div>
+  )
+}
+
+export default App
